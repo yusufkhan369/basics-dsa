d648a26d5ffbe9d27c2b2bf81b547776c6e2be1a
coursera.zam <yusuf47595@gmail.com>
2021-10-08 00:27:32 +0530

    Refactor: Added intial file

---
+++ java/src/crio/programmingconstructs/ReverseNumber.java
@@ -0,0 +1,8 @@
+package crio.programmingconstructs;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        //TODO: Implement the logic to reverse a number
+        System.out.println();
+    }
+}
