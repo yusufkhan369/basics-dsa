5ea20d20b4dc4e4b15cb7b4098810c048db1a22c
coursera.zam <yusuf47595@gmail.com>
2021-10-08 01:11:59 +0530

    intellij

--- java/src/crio/programmingconstructs/ReverseNumber.java
+++ java/src/crio/programmingconstructs/ReverseNumber.java
@@ -3,6 +3,7 @@ package crio.programmingconstructs;
 public class ReverseNumber {
     public static void main(String[] args) {
         //TODO: Implement the logic to reverse a number
+        int number = 123;
         System.out.println();
     }
 }
